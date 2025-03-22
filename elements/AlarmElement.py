08393389ecf07a8c01709746d27d894097a4923a
IlinNR <ilinnr@suek.ru>
2025-03-22 23:38:37 +0300

    allarm ability node added

---
+++ elements/AlarmElement.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class AlarmElement:
+    def __init__(
+        self, 
+        source: str, 
+        anomalies: list, 
+        image: np.ndarray, 
+        timestamp: int | float,
+    ) -> None:
+        self.source = source
+        self.anomalies = anomalies
+        self.image = image
+        self.timestamp = timestamp
--- elements/FrameElement.py
+++ elements/FrameElement.py
@@ -10,22 +10,29 @@ class FrameElement:
         frame_result: np.ndarray,
         timestamp: float,
         frame_number: int,
-        time_date: int,
-        blink: Optional[int] = None,
-        detected_coords: Optional[List[List]] = None,
-        eye_center_coords: Optional[List[List]] = None,
-        blinking_frequency: Optional[float] = None,
-        sleep_status: bool = False,
+        closed_eyes: Optional[int] = None,
+        detected_coords: Optional[List[List]] = [[0, 0],  [0, 0]],
+        eye_center_coords: Optional[List[List]] = [[0, 0],  [0, 0]],   
+        blinking_frequency: Optional[float] = 0.0,
+        sleep_status: int = 0,
+        iris_coords_array: np.ndarray = None, 
+        distance_to_the_object: int = None,
+        yolo_detected_human: list = [0],
+        yolo_detected_gadget: list = None,
+        movement_vectors: list = None,
     ) -> None:
         
         self.source = source
         self.timestamp = timestamp
         self.frame_number = frame_number
-        self.time_date = time_date
-        self.blink = blink
+        self.closed_eyes = closed_eyes
         self.detected_coords = detected_coords
         self.eye_center_coords = eye_center_coords
         self.frame_result = frame_result
         self.blinking_frequency = blinking_frequency
         self.sleep_status = sleep_status
-        self.person_masks = [] # Маски пересекающих людей
+        self.iris_coords_array = iris_coords_array
+        self.distance_to_the_object = distance_to_the_object
+        self.yolo_detected_human = yolo_detected_human
+        self.yolo_detected_gadget = yolo_detected_gadget
+        self.movement_vectors = movement_vectors,
