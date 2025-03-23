8b68a64b5053fe6ea8b74c95e16225173a3e6934
IlinNR <ilinnr@suek.ru>
2025-03-23 21:47:41 +0300

    formated

--- elements/AlarmElement.py
+++ elements/AlarmElement.py
@@ -3,10 +3,10 @@ import numpy as np
 
 class AlarmElement:
     def __init__(
-        self, 
-        source: str, 
-        anomalies: list, 
-        image: np.ndarray, 
+        self,
+        source: str,
+        anomalies: list,
+        image: np.ndarray,
         timestamp: int | float,
     ) -> None:
         self.source = source
