53b6a9465be96d131e5417af0035cdae8283fe2a
IlinNR <ilinnr@suek.ru>
2025-03-31 20:00:04 +0300

    increasing value

--- nodes/VideoReader.py
+++ nodes/VideoReader.py
@@ -25,12 +25,15 @@ class VideoReader:
         self.last_frame_timestamp = -1  # специально отрицательное значение при иницииализации
         self.first_timestamp = 0  # значение  времени в момент первого кадра потока
 
-        self.break_element = False  # проверяет отправлен ли элемент прерывающий видеопотока
-
+        self.act_as_real_rtsp = True
         # устанавливаем ширину и высоту при обработке видео с камеры
         if type(self.video_pth) == int:
             self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
             self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+        self.fps = 60
+        self.increase_brightness = config_VideoReader["increase_brightness"]
+        self.increasing_value = config_VideoReader["increasing_value"]
 
     def process(self) -> Generator[FrameElement, None, None]:
         # номер кадра текущего видео
@@ -40,13 +43,31 @@ class VideoReader:
             ret, frame = self.stream.read()
             if not ret:
                 break
-
+            # frame = cv2.resize(frame, (800, 600))
             timestamp = time.time()  # timestamp
             frame_number += 1
 
             # Пропустим некоторые кадры если требуется согласно конфигу
             if abs(self.last_frame_timestamp - timestamp) < self.skip_secs:
                 continue
+            elif self.act_as_real_rtsp:
+                time.sleep(1 / self.fps)
+
             if self.video_source == 0:
                 self.video_source = "0"
+
+            if self.increase_brightness:
+                # Преобразование в HSV
+                hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+                # Разделение каналов HSV
+                h, s, v = cv2.split(hsv)
+
+                v += self.increasing_value
+                # Объединение обратно в HSV
+                hsv_normalized = cv2.merge([h, s, v])
+
+                # Преобразование обратно в BGR
+                frame = cv2.cvtColor(hsv_normalized, cv2.COLOR_HSV2BGR)
+
             yield FrameElement(self.video_source, frame, timestamp, frame_number)
